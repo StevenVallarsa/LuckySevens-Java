5640ba60aade018322bbdefb3e2cc0f8b609066b
StevenVallarsa <orionsjv@gmail.com>
2022-01-05 21:27:13 -0500

    completed basic functionality

--- src/main/java/com/sv/luckysevens2022/LuckySevens.java
+++ src/main/java/com/sv/luckysevens2022/LuckySevens.java
@@ -5,6 +5,8 @@
 
 package com.sv.luckysevens2022;
 
+import java.util.Random;
+
 /**
  *
  * @author: Steven Vallarsa
@@ -14,8 +16,40 @@ package com.sv.luckysevens2022;
  */
 public class LuckySevens {
     
-    double startingBalance = 100;
-    
-    
+    public static void main(String[] args) {
+        
+        
+        Random random = new Random();
+        
+        int startingBalance = 100;
+        int currentBalance = startingBalance;
+        int max = startingBalance;
+        int countAtMax = 0;
+        int count = 0;
 
+        while (currentBalance > 0) {
+            
+            count++;
+            
+            if (random.nextInt(6) + (random.nextInt(6)) + 2 == 7) {
+                currentBalance += 4;
+            } else {
+                currentBalance--;
+            }
+            
+            if (currentBalance >= max) {
+                max = currentBalance;
+                countAtMax = count;
+            }
+            
+        }
+        
+        System.out.println("You lost $" + startingBalance + " in only " + count + " rounds.");
+        if (countAtMax == 0) {
+            System.out.println("You never broke even.");
+        } else {
+            System.out.printf("You should have stopped at round %d when you were $%d ahead!", countAtMax, max - startingBalance);
+        }
+    }
+    
 }
