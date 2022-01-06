18986b2881516ae0ac228b0684d0977179d404ad
StevenVallarsa <orionsjv@gmail.com>
2022-01-05 20:37:38 -0500

    first commit

---
+++ src/main/java/com/sv/luckysevens2022/LuckySevens.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.sv.luckysevens2022;
+
+/**
+ *
+ * @author: Steven Vallarsa
+ *   email: stevenvallarsa@gmail.com
+ *    date: 2022-00-00
+ * purpose: A small program to prove the futility of gambling with dice
+ */
+public class LuckySevens {
+    
+    double startingBalance = 100;
+    
+    
+
+}
